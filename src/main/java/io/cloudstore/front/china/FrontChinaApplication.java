8ee28ac7034e2a516556f442450ec70d1d7b70c1
chanwook <chanwook.god@gmail.com>
2015-09-24 20:25:17 +0900

    success connect to config server~

--- src/main/java/io/cloudstore/front/china/FrontChinaApplication.java
+++ src/main/java/io/cloudstore/front/china/FrontChinaApplication.java
@@ -1,11 +1,19 @@
+package io.cloudstore.front.china;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 /**
  * @author chanwook
  */
 @SpringBootApplication
 public class FrontChinaApplication {
+
+    @Autowired
+    Environment env;
+
     public static void main(String[] args) {
         SpringApplication.run(FrontChinaApplication.class, args);
     }
