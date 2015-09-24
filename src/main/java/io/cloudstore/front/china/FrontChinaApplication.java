3ca2e3a43b15a45f1dd733e32dfb233247c225a6
chanwook <chanwook.god@gmail.com>
2015-09-25 00:31:37 +0900

    add eureka client

--- src/main/java/io/cloudstore/front/china/FrontChinaApplication.java
+++ src/main/java/io/cloudstore/front/china/FrontChinaApplication.java
@@ -3,12 +3,14 @@ package io.cloudstore.front.china;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.core.env.Environment;
 
 /**
  * @author chanwook
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class FrontChinaApplication {
 
     @Autowired
