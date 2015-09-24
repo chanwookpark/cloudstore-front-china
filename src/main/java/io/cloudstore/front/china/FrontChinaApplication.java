77ff786055106c7a3e54fc8b63b68353ddd01f06
chanwook <chanwook.god@gmail.com>
2015-09-25 01:05:41 +0900

    add to hystrix dashboard

--- src/main/java/io/cloudstore/front/china/FrontChinaApplication.java
+++ src/main/java/io/cloudstore/front/china/FrontChinaApplication.java
@@ -3,7 +3,9 @@ package io.cloudstore.front.china;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.core.env.Environment;
 
 /**
@@ -11,6 +13,8 @@ import org.springframework.core.env.Environment;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
+@EnableHystrixDashboard //TODO 별도 서버로 뽑아내자
 public class FrontChinaApplication {
 
     @Autowired
